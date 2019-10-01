ab784359764935db09cadce4192d0feed6606eec
Joonas Meriläinen <merilainenjoonas@gmail.com>
2019-10-01 17:53:02 +0300

    Add properties to product presenter

--- src/Presenters/ProductPresenter.php
+++ src/Presenters/ProductPresenter.php
@@ -24,4 +24,34 @@ class ProductPresenter extends Presenter
             'secondary' => $this->entity->components()->type('secondary'),
         ];
     }
+
+    public function contract_duration()
+    {
+        return 'Sopimuksen kesto: ' . $this->entity->getContractDuration();
+    }
+
+    public function price_period()
+    {
+        $main = $this->entity->components()->first();
+        $from = $main->get('valid_from')->setTimezone('Europe/Helsinki')->format('j.n.');
+        $to = $main->get('valid_to')->setTimezone('Europe/Helsinki')->subDay()->format('j.n.Y');
+        return "Hintajakso: {$from}-{$to}";
+    }
+
+    public function meta()
+    {
+        $keys = $this->entity->getMeta();
+        return array_map(function($key) {
+            return $this->$key;
+        }, $keys);
+    }
+
+    public function order_link()
+    {
+        return $this->entity->getOrderLink();
+    }
+
+    public function isType($type) {
+        return $this->entity->isType($type);
+    }
 }
