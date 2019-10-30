94a82a443b39b544449a66ff4a6bb9f1ef17021e
Joonas Meriläinen <merilainenjoonas@gmail.com>
2019-10-30 16:25:31 +0200

    Move notice display logic to main action class

--- src/Actions/Notice.php
+++ src/Actions/Notice.php
@@ -13,10 +13,33 @@ class Notice
             return \WP_CLI::$level('[Electro] ' . $message);
         }
 
-        $id = 'electro/notices/'. get_current_user_id();
-        $notices = get_option($id) ?? [];
-        $notices[] = [$message, $level];
-        update_option($id, $notices);
+        if (is_admin()) {
+            $userId = get_current_user_id();
+            $notices = get_user_option($this->action, $userId) ?? [];
+            $notices[] = [$message, $level];
+            update_user_option($userId, $this->action, $notices);
+        }
+    }
+
+    public function displayAdminNotices()
+    {
+        $userId = get_current_user_id();
+        $notices = get_user_option($this->action, $userId);
+
+        if (empty($notices)) {
+            return;
+        }
+
+        foreach($notices as [$message, $level]) {
+            $class = sprintf('notice notice-%s', $level);
+            echo sprintf(
+                '<div class="%1$s"><p>%2$s</p></div>',
+                esc_attr($class),
+                esc_html($message)
+            );
+        }
+
+        delete_user_option($userId, $this->action);
     }
 
     public function info($message) {
--- src/Admin/Notices/Notices.php
+++ src/Admin/Notices/Notices.php
@@ -6,22 +6,6 @@ class Notices
 {
     public function handle()
     {
-        $id = 'electro/notices/'. get_current_user_id();
-        $notices = get_option($id);
-
-        if (empty($notices)) {
-            return;
-        }
-
-        foreach($notices as [$message, $level]) {
-            $class = sprintf('notice notice-%s', $level);
-            echo sprintf(
-                '<div class="%1$s"><p>%2$s</p></div>', 
-                esc_attr($class), 
-                esc_html($message)
-            );  
-        }
-
-        delete_option($id);
+        do_action('electro/notice/displayAdminNotices');
     }
 }
